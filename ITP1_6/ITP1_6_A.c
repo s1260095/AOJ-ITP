a9ecaa6814c00b332add83b90cd6a1aff71826c1
s1260095 <s1260095@u-aizu.ac.jp>
2018-07-26 17:47:22 +0900

    Add solution ITP1_6_A

---
+++ ITP1_6/ITP1_6_A.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int i,m,w,n[105];
+
+  scanf("%d",&m);
+  i=m;
+  for ( i=0;i<m; i++)
+    {
+    scanf("%d",&w);
+  n[i]=w;
+    }
+  for()
+  printf("%2d",n[i]);
+
+                         
+  return 0;
+}
